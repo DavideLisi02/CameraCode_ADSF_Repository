702a79896e17d4755daaf943663752460b0c3153
DavideLisi02 <davide.lisi@epfl.ch>
2024-11-06 14:46:44 +0100

    Code init

---
+++ old_version/camera_pins.h
@@ -0,0 +1,21 @@
+
+
+//=== my pin asssignment =====
+#define PWDN_GPIO_NUM		35
+#define RESET_GPIO_NUM		8
+#define XCLK_GPIO_NUM     	40
+#define SIOD_GPIO_NUM     	42
+#define SIOC_GPIO_NUM     	1
+
+#define Y9_GPIO_NUM       	4
+#define Y8_GPIO_NUM       	16
+#define Y7_GPIO_NUM       	11
+#define Y6_GPIO_NUM       	15
+#define Y5_GPIO_NUM       	13
+#define Y4_GPIO_NUM       	37
+#define Y3_GPIO_NUM       	14
+#define Y2_GPIO_NUM       	36
+#define VSYNC_GPIO_NUM    	9
+#define HREF_GPIO_NUM     	41
+#define PCLK_GPIO_NUM     	3
+//============================
